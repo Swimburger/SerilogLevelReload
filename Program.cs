98e90d460a90b0f8e7b98566329882fc7156be79
Niels Swimberghe <nielsswimberghe@gmail.com>
2020-09-16 16:41:34 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("serilog.json")
+            .Build();
+
+        var log = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        while (true)
+        {
+            log.Verbose("This is a Verbose log message");
+            log.Debug("This is a Debug log message");
+            log.Information("This is an Information log message");
+            log.Warning("This is a Warning log message");
+            log.Error("This is a Error log message");
+            log.Fatal("This is a Fatal log message");
+            await Task.Delay(1000);
+        }
+    }
+}
